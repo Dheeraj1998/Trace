27e23b727f5c04f299d22b258b2df70cce76fc53
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2019-04-04 01:17:42 +0530

    Completed essential features.

--- app/src/main/java/com/example/trace/AnalysisActivity.java
+++ app/src/main/java/com/example/trace/AnalysisActivity.java
@@ -1,7 +1,21 @@
 package com.example.trace;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class AnalysisActivity extends AppCompatActivity {
 
@@ -10,5 +24,47 @@ public class AnalysisActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis);
         setTheme(R.style.AppTheme);
+
+        GraphView graph_1 = findViewById(R.id.graph_1);
+        LineGraphSeries<DataPoint> series_1 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3)
+        });
+        series_1.setAnimated(true);
+        series_1.setThickness(8);
+        series_1.setColor(Color.CYAN);
+
+        graph_1.setTitle("Body Temperature");
+        graph_1.setTitleColor(Color.WHITE);
+        graph_1.getLegendRenderer().setVisible(true);
+        graph_1.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        graph_1.addSeries(series_1);
+
+        GraphView graph_2 = findViewById(R.id.graph_2);
+        BarGraphSeries<DataPoint> series_2 = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 22),
+                new DataPoint(1, 37),
+                new DataPoint(2, 45)
+        });
+        graph_2.setTitle("Stress Levels");
+        graph_2.setTitleColor(Color.WHITE);
+        graph_2.addSeries(series_2);
+
+        GraphView graph_3 = findViewById(R.id.graph_3);
+        LineGraphSeries<DataPoint> series_3 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 5),
+                new DataPoint(1, 2),
+                new DataPoint(2, 3)
+        });
+        series_3.setAnimated(true);
+        series_3.setThickness(8);
+        series_3.setColor(Color.RED);
+
+        graph_3.setTitle("Humidity");
+        graph_3.setTitleColor(Color.WHITE);
+        graph_3.getLegendRenderer().setVisible(true);
+        graph_3.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        graph_3.addSeries(series_3);
     }
 }
--- app/src/main/java/com/example/trace/FamilyActivity.java
+++ app/src/main/java/com/example/trace/FamilyActivity.java
@@ -1,13 +1,129 @@
 package com.example.trace;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class FamilyActivity extends AppCompatActivity {
+
+    private TextView fm_1_1, fm_1_2, fm_1_3, fm_1_4, fm_2_1, fm_2_2, fm_2_3, fm_2_4, fm_3_1, fm_3_2, fm_3_3, fm_3_4, fm_4_1, fm_4_2, fm_4_3, fm_4_4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        fm_1_1 = findViewById(R.id.fm_1_1);
+        fm_1_2 = findViewById(R.id.fm_1_2);
+        fm_1_3 = findViewById(R.id.fm_1_3);
+        fm_1_4 = findViewById(R.id.fm_1_4);
+
+        fm_2_1 = findViewById(R.id.fm_2_1);
+        fm_2_2 = findViewById(R.id.fm_2_2);
+        fm_2_3 = findViewById(R.id.fm_2_3);
+        fm_2_4 = findViewById(R.id.fm_2_4);
+
+        fm_3_1 = findViewById(R.id.fm_3_1);
+        fm_3_2 = findViewById(R.id.fm_3_2);
+        fm_3_3 = findViewById(R.id.fm_3_3);
+        fm_3_4 = findViewById(R.id.fm_3_4);
+
+        fm_4_1 = findViewById(R.id.fm_4_1);
+        fm_4_2 = findViewById(R.id.fm_4_2);
+        fm_4_3 = findViewById(R.id.fm_4_3);
+        fm_4_4 = findViewById(R.id.fm_4_4);
+
+        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
+        Query reqQuery = dbref.child("Abram");
+
+        reqQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot data_pack : dataSnapshot.getChildren()) {
+                        fm_1_1.setText(data_pack.child("Gas").getValue().toString() + " ppm");
+                        fm_1_2.setText(data_pack.child("Humidity").getValue().toString() + " g/m3");
+                        fm_1_3.setText(data_pack.child("Stress").getValue().toString() + " str");
+                        fm_1_4.setText(data_pack.child("Temperature").getValue().toString() + " °C");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        reqQuery = dbref.child("Anay");
+
+        reqQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot data_pack : dataSnapshot.getChildren()) {
+                        fm_4_1.setText(data_pack.child("Gas").getValue().toString() + " ppm");
+                        fm_4_2.setText(data_pack.child("Humidity").getValue().toString() + " g/m3");
+                        fm_4_3.setText(data_pack.child("Stress").getValue().toString() + " str");
+                        fm_4_4.setText(data_pack.child("Temperature").getValue().toString() + " °C");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        reqQuery = dbref.child("Advik");
+
+        reqQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot data_pack : dataSnapshot.getChildren()) {
+                        fm_2_1.setText(data_pack.child("Gas").getValue().toString() + " ppm");
+                        fm_2_2.setText(data_pack.child("Humidity").getValue().toString() + " g/m3");
+                        fm_2_3.setText(data_pack.child("Stress").getValue().toString() + " str");
+                        fm_2_4.setText(data_pack.child("Temperature").getValue().toString() + " °C");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        reqQuery = dbref.child("Akarsh");
+
+        reqQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot data_pack : dataSnapshot.getChildren()) {
+                        fm_3_1.setText(data_pack.child("Gas").getValue().toString() + " ppm");
+                        fm_3_2.setText(data_pack.child("Humidity").getValue().toString() + " g/m3");
+                        fm_3_3.setText(data_pack.child("Stress").getValue().toString() + " str");
+                        fm_3_4.setText(data_pack.child("Temperature").getValue().toString() + " °C");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
